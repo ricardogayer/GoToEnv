7c75d9f0f54317ac61ab5dd73d6da2a25926725d
Ricardo Gayer <ricardo.r.gayer@gmail.com>
2021-12-14 12:44:57 -0300

    comentário no arquivo .env

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()  // este método ler o arquivo .env
-	err = godotenv.Load("prod.env")
+	err := godotenv.Load() // este método ler o arquivo .env
+	// err = godotenv.Load("prod.env")
 	if err != nil {
 		log.Println("Erro lendo as variáveis de ambiente")
 	}
@@ -17,9 +18,14 @@ func main() {
 	url := os.Getenv("DATABASE_URL")
 	user := os.Getenv("DATABASE_USER")
 	password := os.Getenv("DATABASE_PASSWD")
+	linhaInicial := os.Getenv("LINHA_INICIAL")
+	linhaFinal := os.Getenv("LINHA_FINAL")
 
-	fmt.Println("URL: ",url)
-	fmt.Println("User: ",user)
-	fmt.Println("Password: ",password)
-	
+	fmt.Println("URL: ", url)
+	fmt.Println("User: ", user)
+	fmt.Println("Password: ", password)
+
+	fmt.Println("Linha Inicial: ", linhaInicial)
+	fmt.Println("Linha Final: ", linhaFinal)
+
 }
